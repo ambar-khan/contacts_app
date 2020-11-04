5693871b4515f8787c7301ecf2b7b3be48670d0f
Ambar <ambarmogg@me.com>
2020-11-03 17:48:57 -0800

    Json to display first contact info

---
+++ app/controllers/api/contacts_controller.rb
@@ -0,0 +1,8 @@
+class Api::ContactsController < ApplicationController
+
+  def first
+    @contact = Contact.first
+    render 'first_contact.json.jb'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  namespace :api do
+    get "/first_contact" => "contacts#first"
+  end
+  
 end
